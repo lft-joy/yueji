80d03725d933c6a198e746c11604eb307189dc1c
lft <875509000@qq.com>
2019-03-20 19:50:48 +0800

    完成了展示页面的基本设置
    创建了model，并在数据库和网页都能看到文字描述，实现了在服务器端看到图片，还未实现在网页上显示出来。

--- cmdb/models.py
+++ cmdb/models.py
@@ -6,3 +6,8 @@ from django.db import models
 
 class Cmdb(models.Model):
     description = models.CharField(max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='标题', max_length=50)
+
+    def __str__(self):          #这个方法的作用是将你输入的title设置为标题
+        return self.title
